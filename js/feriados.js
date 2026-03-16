0b5995f3f0e4a992efa89cfea30c927a0a31b8f6
vinycius <vinyciusalmeida017@gmail.com>
2026-03-16 04:18:15 -0300

    adiciona legenda de cores ao calendário, atualiza lista de feriados e melhora a lógica de renderização; atualiza versão do Service Worker para 2.9.1

--- js/feriados.js
+++ js/feriados.js
@@ -2,12 +2,19 @@
 const LISTA_INFO_FERIADOS = [
     { data: "01/01", mes: 0, nome: "Confraternização Universal", tipo: "Geral" },
     { data: "17/02", mes: 1, nome: "Carnaval", tipo: "Manutenção" },
-    { data: "03/04", mes: 3, nome: "Sexta-feira Santa", tipo: "Geral" },
+    { data: "03/04", mes: 3, nome: "Paixão de Cristo", tipo: "Geral" },
     { data: "21/04", mes: 3, nome: "Tiradentes", tipo: "Manutenção" },
+    { data: "05/04", mes: 3, nome: "Páscoa", tipo: "Manutenção" },
     { data: "01/05", mes: 4, nome: "Dia do Trabalho", tipo: "Geral" },
     { data: "04/06", mes: 5, nome: "Corpus Christi", tipo: "Manutenção" },
-    { data: "09/07", mes: 6, nome: "Revolução Constitucionalista", tipo: "Manutenção" },
-    { data: "07/09", mes: 8, nome: "Independência do Brasil", tipo: "Geral" }
+    { data: "09/07", mes: 6, nome: "Carta Magna do Estado de S.Paulo", tipo: "Manutenção" },
+    { data: "07/09", mes: 8, nome: "Independência do Brasil", tipo: "Geral" },
+    { data: "12/10", mes: 9, nome: "N.S.Aparecida", tipo: "Manutenção" },
+    { data: "02/11", mes: 10, nome: "Finados", tipo: "Manutenção" },
+    { data: "05/11", mes: 10, nome: "Aniversário da Cidade", tipo: "Geral" },
+    { data: "15/11", mes: 10, nome: "Proclamação da República", tipo: "Manutenção" },
+    { data: "20/11", mes: 10, nome: "Consciência Negra", tipo: "Manutenção" },
+    { data: "25/12", mes: 11, nome: "Natal", tipo: "Geral" }
 ];
 
 function renderizarTabelaFeriados(mesAtual) {
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'folgas-v2.9';
+const CACHE_NAME = 'folgas-v2.9.1';
 const ASSETS = [
   './',
   './index.html',
